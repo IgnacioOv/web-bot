508fc36fb9359a3c7af0cd7a0856027f3ee3f830
IgnacioOv <nachoolivavelez18@gmail.com>
2021-11-19 15:48:49 -0300

    write database with page load data

--- main.py
+++ main.py
@@ -1,34 +1,71 @@
 from selenium import webdriver
-
+from datetime import date, datetime
 import sqlite3
 
 URL = "https://intranet.inkua.de/dashboard"
 
+driver = webdriver.Chrome()
+driver.get(URL)
 
-def connect_site():
-    pass
+
+def get_time():
+    now = datetime.now()
+
+    current_time = now.strftime("%H:%M:%S")
+    return current_time
 
 
-def write_db():
+def get_date():
+    today = date.today()
+    date_data = today.strftime("%d/%m/%Y")
+    return date_data
+
+
+def loading_site():
+    navigationStart = driver.execute_script(
+        "return window.performance.timing.navigationStart"
+    )
+    responseStart = driver.execute_script(
+        "return window.performance.timing.responseStart"
+    )
+    domComplete = driver.execute_script("return window.performance.timing.domComplete")
+
+    backendPerformance = responseStart - navigationStart
+    frontendPerformance = domComplete - responseStart
+
+    print("Back End: %s" % backendPerformance)
+    print("Front End: %s" % frontendPerformance)
+
+    driver.quit()
+    return frontendPerformance
+
+
+def write_db(load, date, time):
     con = sqlite3.connect("./example.db")
     cur = con.cursor()
 
-    # cur.execute("""CREATE TABLE loggins    (date text, time text, delay text)""")
-    cur.execute("""INSERT INTO loggins VALUES ('2006-01-05','12pm','12ms')""")
-    cur.execute("""INSERT INTO loggins VALUES ('2016-01-05','12pm','12ms')""")
+    cur.execute(
+        """CREATE TABLE IF NOT EXISTS loggins    (date text, time text, delay text)"""
+    )
+    cur.execute(
+        """INSERT INTO loggins VALUES (?,?,?)""",
+        (date, time, load),
+    )
+
     for row in cur.execute("SELECT * FROM loggins ORDER BY date"):
         print(row)
 
     con.commit()
     con.close()
-    pass
 
 
 def main():
     # conectar con la pagina
-    print("anda")
-    connect_site()
-    write_db()
+    time = get_time()
+    date = get_date()
+    loading_web = loading_site()
+
+    write_db(loading_web, date, time)
 
 
 main()
